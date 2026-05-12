94171a36aad2870b20829cf9ae3c1703911ab32b
mansiey <mansi46rd@gmail.com>
2026-05-12 19:28:29 +0530

    introduction to DOM

--- 8-Modern Async Patterns/1-closure.js
+++ 8-Modern Async Patterns/1-closure.js
@@ -1,8 +1,8 @@
 //Examples:
 //1.
 
-function startCompany(){
-    function chef(name){
+function startCompany() {
+    function chef(name) {
         return `Name of your company is ${name}`;
     }
     return chef;
@@ -13,12 +13,12 @@ console.log(getMyCompany('FoodLoverss!!'));
 
 
 //2.
-function eternal(guest){
+function eternal(guest) {
     const guestName = guest;
     let count = 0;
 
     function zomato() {
-        if(count === 1) return;
+        if (count === 1) return;
         console.log(`Hi ${guestName}, from zomato!`);
         count++;
     }
@@ -29,7 +29,7 @@ function eternal(guest){
 
     // zomato();
     // blinkit();
-    return{
+    return {
         zomato,
         blinkit,
     }
@@ -40,4 +40,62 @@ const suraj = eternal('Suraj');
 
 mansi.zomato();
 mansi.zomato();
-mansi.blinkit(); 
+mansi.blinkit();
+
+
+
+//PIYUSH SIR'S NOTES
+
+function add(a, b) {
+    return (a + b);
+}
+
+console.log(add(3, 4));       //this is not optimized for large no. of function calls
+
+
+//Writing a function that optimizes any calculating function:
+
+function createOptimizedVersion(fn) {
+    const cache = {};
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache[key]) {
+            return cache[key];
+        }
+
+        const result = fn(...args);
+        cache[key] = result;
+
+        return result;
+    }
+}
+
+const optimizedAdd = createOptimizedVersion(add);
+console.log(optimizedAdd(4,5));
+
+
+console.time('start');
+add(3,4)
+console.timeEnd('start');
+console.time('start');
+add(3,4)
+console.timeEnd('start');
+console.time('start');
+add(3,4)
+console.timeEnd('start');
+
+
+console.time('start');
+(optimizedAdd(4,5));
+console.timeEnd('start');
+console.time('start');
+(optimizedAdd(4,5));
+console.timeEnd('start');
+console.time('start');
+(optimizedAdd(4,5));
+console.timeEnd('start');
+console.time('start');
+(optimizedAdd(4,5));
+console.timeEnd('start');
+
